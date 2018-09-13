d79d6bb6ab703b9d75e0a9dc2af5d27602801586
Daniel Campbell <dcampbell1@laurentian.ca>
2018-09-13 09:02:58 -0400

    added new label

--- GitHubAddNewProject/Form1.cs
+++ GitHubAddNewProject/Form1.cs
@@ -16,5 +16,10 @@ namespace GitHubAddNewProject
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
